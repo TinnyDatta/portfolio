6e7e3e5e6c02a2d47664bca82b65f4c9973ed644
TinnyDatta <tinnydatta.prof@gmail.com>
2024-07-04 11:28:55 +0600

    added latest education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,20 +1,17 @@
 
 const Education = () => {
     return (
-        <div>
-            <h2 className="relative text-center text-2xl font-bold mt-10 mb-3 ">My Education</h2>
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
-	<div className="mt-6 mb-2 bg-black text-white p-6">
-    <div className="">
-		<span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-600">Quisque</span>
-		<h2 className="text-xl font-semibold tracking-wide">Nam maximus purus</h2>
-	</div>
-	<p>Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
-    </div>
-</div>
-            </div>
-        </div>
+         <>
+          <h2 className="relative text-center text-2xl font-bold mt-10 mb-3 ">Education</h2>
+         <div className="bg-blue-100">
+         <div className="my-10 text-center text-2xl">
+         <h2 className=" text-black pb-2 pt-5" >Bachelor at English</h2>
+         <p className="pb-2 text-black">Moulvibazar Govt. Womens College</p>
+         <p className="pb-5 text-black">2020 - Present</p>
+         </div>
+         </div>
+         </>
+        
     );
 };
 
